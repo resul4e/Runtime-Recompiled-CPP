da7bde01cb744d07225c79d4880ae8647bad992c
Resul Celik <resul_celik@hotmail.com>
2020-04-15 16:26:19 +0200

    Removed parameter we don't need anymore in setup.py.

--- Engine/Tools/Setup.py
+++ Engine/Tools/Setup.py
@@ -3,8 +3,7 @@ import sys
 import os
 
 project_configuration=sys.argv[1]
-project_platform=sys.argv[2]
-game_path=sys.argv[3]
+game_path=sys.argv[2]
 
 if not os.path.isdir(game_path+"/Scripts/Intermediate/Objects/" + project_configuration):
 	os.makedirs(game_path+"/Scripts/Intermediate/Objects/" + project_configuration)
