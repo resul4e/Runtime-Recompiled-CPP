591dac825ccc3854e2631d97cb8d9e374ad798bb
Resul Çelik <resul_celik@hotmail.com>
2020-07-16 20:32:14 +0200

    Added first draft of compile script for linux.

---
+++ Engine/Tools/Linux/Compile.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import subprocess
+import sys
+import os
+
+project_configuration=sys.argv[1]
+project_platform=sys.argv[2]
+game_path=sys.argv[3]
+script_path=sys.argv[4]
+scriptName=sys.argv[5]
+VSSE=sys.argv[6]
+
+
+# if project_configuration == "Release":
+# 	configuration_dependant = "/GL /O2 /Oi /LD /Zi"
+# else:
+# 	configuration_dependant = "/ZI /LDd"
+
+#run_command = VSInstallDir +"\\VC\\Auxiliary\\Build\\vcvarsall.bat x" + project_platform
+#run_command += "&& cl "+configuration_dependant+" /MP
+#
+#run_command += "/Fd"+game_path+"/Scripts\\Intermediate\\"+scriptName+".pdb /showIncludes /nologo -I"+VSSE+" -I"+VSSE+"/Plugins -I"+VSSE+"/Extern/glm -I"+VSSE+"/Extern/spdLog/include -I"+VSSE+"/PIL "+script_path
+
+run_command = "g++ -c " + script_path + " -o " + game_path + "/Scripts/Intermediate/Objects/"+scriptName+".o" " -I"+VSSE+" -I"+VSSE+"/Plugins -I"+VSSE+"/Extern/glm -I"+VSSE+"/Extern/spdLog/include -I"+VSSE+"/PIL -std=c++17"
+os.system(run_command)
+
+
+
+
+#
